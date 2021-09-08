e3f39a35ebf45e24862306d5b4fa4ab2f6f1db07
Lkhagva-erdene <anidas.com@gmail.com>
2021-09-08 18:50:47 +0800

    with layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layouts from "../component/Layouts";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Layouts>
+        <Component {...pageProps} />
+      </Layouts>
+  )
 }
 export default MyApp
